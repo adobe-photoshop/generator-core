e12178436aa861e0b3ab1ab45340a4ec1a476272
Joel Brandt <joelrbrandt@gmail.com>
2013-06-13 10:48:30 -0700

    jshint and comment cleanup

--- lib/jsx/networkEventSubscribe.jsx
+++ lib/jsx/networkEventSubscribe.jsx
@@ -5,7 +5,7 @@
 
 var i, actionDescriptor;
 actionDescriptor = new ActionDescriptor();
-actionDescriptor.putString( stringIDToTypeID( "version" ), "1.0.0" ); 
+actionDescriptor.putString(stringIDToTypeID("version"), "1.0.0");
 for (i = 0; i < params.events.length; i++) {
     actionDescriptor.putClass(stringIDToTypeID("eventIDAttr"), stringIDToTypeID(params.events[i]));
     executeAction(stringIDToTypeID("networkEventSubscribe"), actionDescriptor, DialogModes.NO);
