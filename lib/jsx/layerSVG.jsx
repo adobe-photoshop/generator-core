82d9eb459eabb978994b634592dc2c65dc8c57a4
jpeterso <jp@adobe.com>
2013-07-17 18:44:21 -0700

    Accept filename parameter for SVG files.

--- lib/jsx/layerSVG.jsx
+++ lib/jsx/layerSVG.jsx
@@ -2,11 +2,12 @@
 
 // Expected params:
 //   - layerID: ID of layer to generate SVG info for
+//   - layerFilename: name to write the SVG file to.
 
 // "File" is an ExtendScript global that does not require the use of "new".
 var appFolder = { Windows: "/", Macintosh: "/Adobe Photoshop CC.app/Contents/" };
 $.evalFile(app.path + appFolder[File.fs] + localize("$$$/ScriptingSupport/Required=Required") + "/ConvertSVG.jsx");
 
 /*jshint newcap: false */
-svg.generateFileByID(params.layerID);
+svg.generateFileByID(params.layerID, params.layerFilename);
 /*jshint newcap: true */
