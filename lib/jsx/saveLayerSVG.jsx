a4bf8d2d67a88c8f71f5a66b5488ef8f9022b365
Joel Brandt <joelrbrandt@gmail.com>
2013-07-28 08:55:55 -0700

    add API to expose SVG creation, clean up JSX that initiates SVG creation

--- lib/jsx/saveLayerSVG.jsx
+++ lib/jsx/saveLayerSVG.jsx
@@ -5,10 +5,7 @@
 //   - layerFilename: name to write the SVG file to.
 //   - layerScale: amount to scale SVG (value of 1 generates no scaling code).
 
-// "File" is an ExtendScript global that does not require the use of "new".
 var appFolder = { Windows: "/", Macintosh: "/Adobe Photoshop CC.app/Contents/" };
 $.evalFile(app.path + appFolder[File.fs] + localize("$$$/ScriptingSupport/Required=Required") + "/ConvertSVG.jsx");
 
-/*jshint newcap: false */
 svg.generateFileByID(params.layerID, params.layerFilename, params.layerScale);
-/*jshint newcap: true */
