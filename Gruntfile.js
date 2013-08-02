ef72e422489227fc8071765c751e08d18a598199
Joel Brandt <joelrbrandt@gmail.com>
2013-08-02 15:42:52 -0700

    configure travis ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-nodeunit");
 
-    grunt.registerTask("default", ["jshint", "nodeunit"]);
+    grunt.registerTask("test", ["jshint", "nodeunit"]);
+
+    grunt.registerTask("default", ["test"]);
         
     grunt.registerTask("package", "Create distributable Generator package with plugins", function () {
         grunt.log.writeln("Not yet implemented");
