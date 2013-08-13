95dc890994a6cf773b3b6ab7c008473a6ae915a7
Dennis Kehrig <dkehrig@adobe.com>
2013-08-13 16:57:43 -0700

    Also log OS information

--- app.js
+++ app.js
@@ -191,9 +191,12 @@
     }
     
     function init() {
+        var os = require("os");
+
         // Record command line arguments
         console.log("[init] node version: %j", process.versions);
         console.log("[init] platform: %j", process.platform);
+        console.log("[init] OS: %s %s (%s)", os.type(), os.release(), os.arch());
         require("./lib/git").logGitInformation("[init]", __dirname);
         console.log("[init] unparsed command line: %j", process.argv);
         console.log("[init] parsed command line: %j", argv);
