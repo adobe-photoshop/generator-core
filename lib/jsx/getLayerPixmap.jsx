2a26f95f81f661de5fe60ee82344901ad246f69e
Dennis Kehrig <dkehrig@adobe.com>
2013-08-23 12:55:22 -0700

    Provide a utility function to ease the task of scaling assets exactly

--- lib/jsx/getLayerPixmap.jsx
+++ lib/jsx/getLayerPixmap.jsx
@@ -82,8 +82,7 @@ actionDescriptor.putEnumerated(
 
 if (params.boundsOnly) {
     actionDescriptor.putBoolean(stringIDToTypeID("boundsOnly"), params.boundsOnly);
-} else if (params.bounds) {
-    actionDescriptor.putBoolean(stringIDToTypeID("bounds"), params.bounds);
 }
+actionDescriptor.putBoolean(stringIDToTypeID("bounds"), params.bounds);
 
 executeAction(stringIDToTypeID("sendLayerThumbnailToNetworkClient"), actionDescriptor, DialogModes.NO);
