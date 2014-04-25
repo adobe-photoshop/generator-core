222833f229b01fde8fbcc2049a9cf5e591a9fec0
marekhrabe <marekhrabe@abdoc.net>
2014-04-25 19:00:39 +0200

    Changed api of getting guides, moved parsing logic to node, made documentId required

--- lib/jsx/getGuides.jsx
+++ lib/jsx/getGuides.jsx
@@ -1,35 +1,36 @@
 /*global preferences, Units, app, params, Direction */
 
-// Optional params:
+// Required params:
 //   - documentId - and id of document to get guides for
 
 var originalUnits = preferences.rulerUnits;
 preferences.rulerUnits = Units.PIXELS;
 
 var guides = [];
-if (params.documentId) {
-    for (var i = 0; i < app.documents.length; i++) {
-        if (app.documents[i].id === params.documentId) {
-            guides = app.documents[i].guides;
-        }
+for (var i = 0; i < app.documents.length; i++) {
+    if (app.documents[i].id === params.documentId) {
+        guides = app.documents[i].guides;
     }
-} else {
-    guides = app.activeDocument.guides;
 }
 
 preferences.rulerUnits = originalUnits;
 
-var result = [];
+var horizontal = [];
+var vertical = [];
 
 var guide;
+var coordinate;
 for (var i = 0; i < guides.length; ++i) {
     guide = guides[i];
-    result.push("{" +
-        "\"coordinate\":" + parseFloat(guide.coordinate, 10) + "," +
-        "\"direction\":\"" + (guide.direction === Direction.HORIZONTAL ? "h" : "v") +
-    "\"}");
+    coordinate = parseFloat(guide.coordinate);
+
+    if (guide.direction === Direction.HORIZONTAL) {
+        horizontal.push(coordinate);
+    } else {
+        vertical.push(coordinate);
+    }
 }
 
 /* jshint -W030 */
 
-"[" + (result.join(",")) + "]";
+horizontal.join(":") + ";" + vertical.join(":");
